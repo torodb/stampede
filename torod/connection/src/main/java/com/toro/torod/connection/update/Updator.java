a17193db0245a60aad04e628ef2951b4fb184cf8
Gonzalo Ortiz Jaureguizar <gortiz@8kdata.com>
2014-10-31 15:05:10 +0100

    [torod-connection] 	+ Removed some backup files 	+ Removed repeted license headers 	+ Fixed an error when compiled with jdk 6

--- torod/connection/src/main/java/com/toro/torod/connection/update/Updator.java
+++ torod/connection/src/main/java/com/toro/torod/connection/update/Updator.java
@@ -20,29 +20,6 @@
 
 package com.toro.torod.connection.update;
 
-/*
- * #%L
- * Torod: Connection
- * $Id:$
- * $HeadURL:$
- * %%
- * Copyright (C) 2014 8Kdata
- * %%
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * #L%
- */
-
 import com.torodb.kvdocument.values.ObjectValue;
 import com.torodb.torod.core.config.DocumentBuilderFactory;
 import com.torodb.torod.core.connection.UpdateResponse;
