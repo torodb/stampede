b9a31a16d08b6fb054ed7cb307d0c034b6679cfa
jerolba <jerolba@gmail.com>
2016-05-31 14:08:52 +0200

    Created Json to KVValue helper class

---
+++ kvdocument/json-converter/src/main/java/com/torodb/kvdocument/conversion/json/MapToKVValueConverter.java
@@ -0,0 +1,117 @@
+package com.torodb.kvdocument.conversion.json;
+
+import java.time.Instant;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.time.temporal.TemporalAccessor;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import com.torodb.common.util.HexUtils;
+import com.torodb.kvdocument.values.KVBoolean;
+import com.torodb.kvdocument.values.KVDocument;
+import com.torodb.kvdocument.values.KVDouble;
+import com.torodb.kvdocument.values.KVInstant;
+import com.torodb.kvdocument.values.KVInteger;
+import com.torodb.kvdocument.values.KVLong;
+import com.torodb.kvdocument.values.KVNull;
+import com.torodb.kvdocument.values.KVValue;
+import com.torodb.kvdocument.values.heap.ByteArrayKVMongoObjectId;
+import com.torodb.kvdocument.values.heap.InstantKVInstant;
+import com.torodb.kvdocument.values.heap.ListKVArray;
+import com.torodb.kvdocument.values.heap.LongKVInstant;
+import com.torodb.kvdocument.values.heap.MapKVDocument;
+import com.torodb.kvdocument.values.heap.StringKVString;
+
+public class MapToKVValueConverter {
+
+	public KVDocument convert(Map<String, Object> source) {
+		return (KVDocument) convertMap(source);
+	}
+
+	private KVValue<?> convertMap(Map<String, Object> source) {
+		if (isSpecialObject(source)) {
+			return buildSpecialObject(source);
+		}
+		LinkedHashMap<String, KVValue<?>> docHM = new LinkedHashMap<>();
+		for (String key : source.keySet()) {
+			String interned = key.intern();
+			Object value = source.get(interned);
+			docHM.put(interned, convertValue(value));
+		}
+		return new MapKVDocument(docHM);
+	}
+
+	private KVValue<?> convertValue(Object value) {
+		if (value == null) {
+			return KVNull.getInstance();
+		}
+		Class fieldClass = value.getClass();
+		if (value instanceof Map) {
+			return convertMap((Map) value);
+		} else if (value instanceof List) {
+			return convertList((List) value);
+		} else if (value instanceof String) {
+			return new StringKVString((String) value);
+		} else if (value instanceof Integer) {
+			return KVInteger.of((Integer) value);
+		} else if (value instanceof Double) {
+			return KVDouble.of((Double) value);
+		} else if (value instanceof Long) {
+			return KVLong.of((Long) value);
+		} else if (value instanceof Boolean) {
+			return KVBoolean.from((boolean) value);
+		} else {
+			throw new RuntimeException("Unexpected type value");
+		}
+	}
+
+	private boolean isSpecialObject(Map<String, Object> map) {
+		if (map != null && map.keySet().size() == 1) {
+			String key = map.keySet().iterator().next();
+			if (key.startsWith("$") && map.get(key) != null) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private KVValue<?> buildSpecialObject(Map<String, Object> map) {
+		String key = map.keySet().iterator().next();
+		Object value = map.get(key);
+		if ("$oid".equals(key) && value instanceof String) {
+			return new ByteArrayKVMongoObjectId(HexUtils.hex2Bytes((String) value));
+		}
+		if ("$date".equals(key)) {
+			return parseDate(key, value);
+		}
+		throw new RuntimeException("Unexpected special object type: " + key);
+	}
+
+	private KVInstant parseDate(String key, Object value) {
+		if ("$date".equals(key) && value instanceof String) {
+			try {
+				DateTimeFormatter sdf = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSX");
+				TemporalAccessor date = sdf.parse((String) value);
+				return new InstantKVInstant(Instant.from(date));
+			} catch (DateTimeParseException e) {
+				e.printStackTrace();
+				throw new RuntimeException("Unexpected error parsing date");
+			}
+		}
+		if ("$date".equals(key) && value instanceof Long) {
+			return new LongKVInstant((Long) value);
+		}
+		if ("$date".equals(key) && value instanceof Double) {
+			return new LongKVInstant(((Double) value).longValue());
+		}
+		throw new RuntimeException("Unexpected date object type: " + key);
+	}
+
+	private KVValue<?> convertList(List<?> values) {
+		List<KVValue<?>> kvvalues = values.stream().map(this::convertValue).collect(Collectors.toList());
+		return new ListKVArray(kvvalues);
+	}
+}
---
+++ kvdocument/json-converter/src/test/java/com/torodb/kvdocument/conversion/json/TestConvertJson.java
@@ -0,0 +1,132 @@
+package com.torodb.kvdocument.conversion.json;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import com.torodb.kvdocument.values.KVArray;
+import com.torodb.kvdocument.values.KVDocument;
+import com.torodb.kvdocument.values.KVInstant;
+import com.torodb.kvdocument.values.KVMongoObjectId;
+import com.torodb.kvdocument.values.KVNull;
+import com.torodb.kvdocument.values.KVValue;
+
+@RunWith(Parameterized.class)
+public class TestConvertJson {
+
+	private JsonParser parser;
+
+	@Parameterized.Parameters
+	public static Collection<JsonParser> parsers() {
+		return Arrays.asList(new JacksonJsonParser(), new GsonJsonParser());
+	}
+
+	public TestConvertJson(JsonParser parser) {
+		this.parser = parser;
+	}
+
+	@Test
+	public void parseEmptyJson() {
+		KVDocument doc = parser.createFromResource("Empty.json");
+		assertEquals(0, doc.size());
+	}
+
+	@Test
+	public void parseField() {
+		KVDocument doc = parser.createFromResource("OneField.json");
+		assertEquals(1, doc.size());
+		assertTrue(doc.containsKey("name"));
+		assertEquals("John", doc.get("name").getValue());
+	}
+
+	@Test
+	public void parseMultipleFields() {
+		KVDocument doc = parser.createFromResource("MultipleFields.json");
+		assertEquals(2, doc.size());
+		assertTrue(doc.containsKey("name"));
+		assertEquals("John", doc.get("name").getValue());
+		assertTrue(doc.containsKey("surename"));
+		assertEquals("Snow", doc.get("surename").getValue());
+	}
+
+	@Test
+	public void parse_id() {
+		KVDocument doc = parser.createFromResource("_idField.json");
+		assertTrue(doc.containsKey("_id"));
+		KVValue<?> id = doc.get("_id");
+		assertTrue(id instanceof KVMongoObjectId);
+		assertEquals("55129FF25916F02D31387E1C", id.toString());
+	}
+
+	@Test
+	public void parseDate() {
+		KVDocument doc = parser.createFromResource("DateField.json");
+		assertTrue(doc.containsKey("creation"));
+		KVValue<?> creation = doc.get("creation");
+		assertTrue(creation instanceof KVInstant);
+	}
+
+	@Test
+	public void parseNullField() {
+		KVDocument doc = parser.createFromResource("NullField.json");
+		assertTrue(doc.containsKey("age"));
+		assertEquals(KVNull.getInstance(), doc.get("age"));
+	}
+
+	@Test
+	public void parseSubDocument() {
+		KVDocument doc = parser.createFromResource("SubDocument.json");
+		assertTrue(doc.containsKey("address"));
+		assertTrue(doc.get("address") instanceof KVDocument);
+	}
+
+	@Test
+	public void parseEmptyArray() {
+		KVDocument doc = parser.createFromResource("EmptyArray.json");
+		assertTrue(doc.containsKey("department"));
+		assertTrue(doc.get("department") instanceof KVArray);
+		assertTrue(((KVArray) doc.get("department")).size() == 0);
+	}
+
+	@Test
+	public void parseArrayWithScalar() {
+		KVDocument doc = parser.createFromResource("ArrayWithScalar.json");
+		KVArray array = (KVArray) doc.get("day");
+		assertEquals(1, array.size());
+		KVValue<?> kvValue = array.get(0);
+		assertEquals("monday", kvValue.getValue());
+	}
+
+	@Test
+	public void parseArrayWithDocument() {
+		KVDocument doc = parser.createFromResource("ArrayWithDocument.json");
+		KVArray array = (KVArray) doc.get("department");
+		assertEquals(1, array.size());
+		assertTrue(array.get(0) instanceof KVDocument);
+	}
+
+	@Test
+	public void parseMultiArrayEmpty() {
+		KVDocument doc = parser.createFromResource("MultiArrayEmpty.json");
+		KVArray array = (KVArray) doc.get("months");
+		assertEquals(1, array.size());
+		assertTrue(array.get(0) instanceof KVArray);
+		KVArray inner = (KVArray) array.get(0);
+		assertEquals(0, inner.size());
+	}
+
+	@Test
+	public void parseMultiArrayWithValue() {
+		KVDocument doc = parser.createFromResource("MultiArray.json");
+		KVArray array = (KVArray) doc.get("months");
+		KVArray inner = (KVArray) array.get(0);
+		assertEquals("April", inner.get(0).getValue());
+	}
+
+}
