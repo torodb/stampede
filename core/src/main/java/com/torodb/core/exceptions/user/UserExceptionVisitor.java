ec2db7e327d32ee3f1887539a1488b21ceb9cf54
Matteo Melli <matteom@8kdata.com>
2016-06-15 09:05:40 +0200

    make UserException abstract
    Also added vistor pattern to UserException

--- core/src/main/java/com/torodb/core/exceptions/user/UserExceptionVisitor.java
+++ core/src/main/java/com/torodb/core/exceptions/user/UserExceptionVisitor.java
@@ -18,31 +18,8 @@
  *     
  */
 
-
 package com.torodb.core.exceptions.user;
 
-import com.torodb.core.exceptions.ToroException;
+public interface UserExceptionVisitor {
 
-/**
- *
- */
-public abstract class UserException extends ToroException {
-    private static final long serialVersionUID = 1L;
-
-    public UserException() {
-    }
-
-    public UserException(String message) {
-        super(message);
-    }
-
-    public UserException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public UserException(Throwable cause) {
-        super(cause);
-    }
-    
-    public abstract <Result, Argument> Result accept(UserExceptionVisitor visitor, Argument arg);
 }
