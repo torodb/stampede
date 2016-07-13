c00dff1a2a4640b20a66737fbed75168c5fe2fe9
jerolba <jerolba@gmail.com>
2016-07-13 18:09:56 +0200

    Fix findbugs errors on KVDocument: Gson Converter

--- kvdocument/json-converter/src/main/java/com/torodb/kvdocument/conversion/json/JacksonJsonParser.java
+++ kvdocument/json-converter/src/main/java/com/torodb/kvdocument/conversion/json/JacksonJsonParser.java
@@ -13,9 +13,9 @@ import com.torodb.kvdocument.values.KVDocument;
 @SuppressWarnings("unchecked")
 public class JacksonJsonParser implements JsonParser {
 
-	private static ObjectMapper mapper = new ObjectMapper();
-
-	private static MapToKVValueConverter converter = new MapToKVValueConverter();
+	private static final ObjectMapper mapper = new ObjectMapper();
+	private static final MapToKVValueConverter converter = new MapToKVValueConverter();
+	private static final TypeReference<List<HashMap<String,Object>>> typeReference = new TypeReference<List<HashMap<String, Object>>>() {};
 
     @Override
     public KVDocument createFromJson(String json) {
@@ -29,7 +29,7 @@ public class JacksonJsonParser implements JsonParser {
     @Override
     public List<KVDocument> createListFromJson(String json) {
         try {
-            return converter.convert((List<Map<String, Object>>) mapper.readValue(json, new TypeReference<List<HashMap<String, Object>>>() {}));
+            return converter.convert((List<Map<String, Object>>) mapper.readValue(json, typeReference));
         } catch (IOException e) {
             throw new RuntimeException("Unparseable document: " + json);
         }
@@ -47,7 +47,7 @@ public class JacksonJsonParser implements JsonParser {
     @Override
     public List<KVDocument> createListFrom(InputStream is) {
         try {
-            return converter.convert((List<Map<String, Object>>) mapper.readValue(is, new TypeReference<List<HashMap<String, Object>>>() {}));
+            return converter.convert((List<Map<String, Object>>) mapper.readValue(is, typeReference));
         } catch (IOException e) {
             throw new RuntimeException("Unparseable document from InputStream", e);
         }
