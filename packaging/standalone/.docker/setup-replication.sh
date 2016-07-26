01514004db1d2f9dbae01ebe06a028e2c2e67b0c
Matteo Melli <matteom@8kdata.com>
2016-07-26 17:20:35 +0200

    Added replication support to torodb docker image

---
+++ packaging/standalone/.docker/setup-replication.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+echo -n "Waiting MongoDB..."
+max_seconds=15
+while ! /usr/bin/mongo mongo:27017/test --eval true > /dev/null 2>&1
+do
+    sleep 1
+    echo -n .
+    max_seconds=$((max_seconds-1))
+    if [ "$max_seconds" -le 0 ]
+    then
+        echo timeout
+        exit 1
+    fi
+done
+echo ok
+
+echo -n "Setting up MongoDB..."
+/usr/bin/mongo  mongo:27017/test --eval 'rs.initiate({_id:"rs1",members:[{_id:0,host:"localhost:27017"}]})'
+echo ok
