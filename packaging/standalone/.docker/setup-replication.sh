942c6be5b7550dd599501e41840e4efeec356422
Matteo Melli <matteom@8kdata.com>
2016-08-05 13:19:00 +0200

    Fixed setup replication for docker image

--- packaging/standalone/.docker/setup-replication.sh
+++ packaging/standalone/.docker/setup-replication.sh
@@ -20,8 +20,9 @@ echo ok
 echo -n "Setting up MongoDB..."
 /opt/mongodb/bin/mongo  mongo:27017/local --eval 'rs.initiate({_id:"rs1",members:[{_id:0,host:"localhost:27017"}]})' > /dev/null 2>&1
 max_seconds=15
-while ! /opt/mongodb/bin/mongo mongo:27017/local --eval 'assert(db.isMaster().ismaster)' > /dev/null 2>1
+while ! /opt/mongodb/bin/mongo mongo:27017/local --eval 'assert(db.isMaster().ismaster)' > /dev/null 2>&1
 do
+    sleep 1
     echo -n .
     max_seconds=$((max_seconds-1))
     if [ "$max_seconds" -le 0 ]
