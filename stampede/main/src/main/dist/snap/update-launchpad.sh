1295b4482ee4a5ba0dfa7ee8429ed8895a5521f1
Matteo Melli <matteom@8kdata.com>
2017-03-17 14:36:08 +0100

    Nightly Builds

--- stampede/main/src/main/dist/snap/update-launchpad.sh
+++ stampede/main/src/main/dist/snap/update-launchpad.sh
@@ -13,11 +13,12 @@ then
 fi
 git init
 git checkout -b $SNAP_CHANNEL
+ssh -v -o BatchMode=yes -o StrictHostKeyChecking=no $LAUNCHPAD_USER@$LAUNCHPAD_HOST true || true
 git remote add origin git+ssh://$LAUNCHPAD_USER@$LAUNCHPAD_HOST/~$LAUNCHPAD_USER/+git/$PACKAGE_NAME-snap
 if ! git ls-remote|grep -q refs/heads/$SNAP_CHANNEL
 then
     git commit --allow-empty -m "init"
-    git push  --set-upstream origin $SNAP_CHANNEL
+    git push --set-upstream origin $SNAP_CHANNEL
 fi
 git fetch
 git checkout `git log --pretty=oneline origin/$SNAP_CHANNEL | tail -1 | sed 's/ .*$//'`
