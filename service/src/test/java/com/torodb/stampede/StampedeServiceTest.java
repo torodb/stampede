b536f649db826cb2d79cc8148ba348c7b9e1fdea
Matteo Melli <matteom@8kdata.com>
2017-06-20 11:34:24 +0200

    Support to specify multiple sync sources

--- service/src/test/java/com/torodb/stampede/StampedeServiceTest.java
+++ service/src/test/java/com/torodb/stampede/StampedeServiceTest.java
@@ -17,6 +17,7 @@
  */
 package com.torodb.stampede;
 
+import com.google.common.collect.ImmutableList;
 import com.google.common.net.HostAndPort;
 import com.google.common.util.concurrent.Service;
 import com.google.inject.Guice;
@@ -30,7 +31,7 @@ import com.torodb.core.logging.DefaultLoggerFactory;
 import com.torodb.mongodb.repl.ConsistencyHandler;
 import com.torodb.mongodb.repl.filters.ReplicationFilters;
 import com.torodb.mongodb.repl.sharding.MongoDbShardingConfig;
-import com.torodb.mongowp.client.wrapper.MongoClientConfiguration;
+import com.torodb.mongowp.client.wrapper.MongoClientConfigurationProperties;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
@@ -91,9 +92,7 @@ public class StampedeServiceTest {
 
     for (int i = 0; i < size; i++) {
       final int id = i;
-      final MongoClientConfiguration mcc = MongoClientConfiguration.unsecure(
-          HostAndPort.fromParts("localhost", 27017 + i)
-      );
+      final MongoClientConfigurationProperties mccp = MongoClientConfigurationProperties.unsecure();
 
       StampedeConfig.ShardConfigBuilder scb = new StampedeConfig.ShardConfigBuilder() {
         @Override
@@ -106,7 +105,8 @@ public class StampedeServiceTest {
             ConsistencyHandler consistencyHandler) {
           return new MongoDbShardingConfig.ShardConfig(
               getShardId(),
-              mcc,
+              ImmutableList.of(HostAndPort.fromParts("localhost", 27017 + id)),
+              mccp,
               "replSet",
               consistencyHandler
           );
